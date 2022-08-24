3fe3eff92a8c95cf45087889b4cbcc7fb41550fc
Student User <student@workstation.lab.example.com>
2022-08-24 11:39:50 -0400

    src changed

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
 print "A chnage is coming !\n";
-=======
 print "A change is in the air!\n";
->>>>>>> 5ab1eb232a731782b6100af4dda71c0fe973af69
 ?>
